d94b36f6d689810e1e647d19a6d36f836635d102
indrakumar <indrakumar.ethiraju@payground.com>
2023-02-05 22:26:02 -0500

    sql challenge create address table and insert address records

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -7,7 +7,25 @@ create table User
     password  varchar(255)        not null -- WHAT!? NOT ENCRYPTED!? ;-)
 );
 
+create table Address
+(
+    user_foreign_id       bigint               not null primary key,
+    address1 varchar(255)        not null,
+    address2  varchar(255),
+    city      varchar(255)       not null,
+    state varchar(100)           not null,
+    postal varchar(10)           not null,
+    FOREIGN KEY (user_foreign_id)
+        REFERENCES user (id)
+        ON DELETE CASCADE
+);
+
+
 insert into User
     (id, firstName, lastName, username, password)
 values (1, 'Phil', 'Ingwell', 'PhilIngwell', 'Password123') ,
     (2, 'Anna', 'Conda', 'AnnaConda', 'Password234');
+
+insert into Address
+    (user_foreign_id, address1, address2, city, state, postal)
+values ( 1, 'adds 1', 'adds 2', 'canton', 'mi', 48185-000);
